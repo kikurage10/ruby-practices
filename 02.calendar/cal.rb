c3c17a88224a71de0d908c5953ce2c648f874b0a
kikurage10 <155941298+kikurage10@users.noreply.github.com>
2024-03-12 15:04:17 +0900

    カレンダーのプログラムを作成

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'date'
+
+opt = OptionParser.new
+params = {}
+opt.on('-m month', Integer) {|month| params[:month] = month}
+opt.on('-y year', Integer) {|year| params[:year] = year}
+opt.parse!(ARGV)
+
+if params[:month]
+  month = params[:month]
+else
+  month = Date.today.month
+end
+if params[:year]
+  year = params[:year]
+else
+  year = Date.today.year
+end
+
+monthfirst = Date.new(year, month, 1)
+monthlast = Date.new(year, month, -1)
+
+monthyear = month.to_s + "月 " + year.to_s
+weeks = "日 月 火 水 木 金 土"
+
+if monthfirst.wday == 0
+  puts monthyear.center(20)
+  print weeks
+else
+  puts monthyear.center(20)
+  puts weeks
+end
+
+monthfirst_blank = "   " * monthfirst.wday
+print monthfirst_blank
+
+def DateCalendar(first, last)
+  today = Date.today
+  (first..last).each do |x|
+    puts if x.wday == 0
+    if x == today
+      print "\e[30m\e[47m" + x.day.to_s + "\e[0m" + " "
+    else
+      print x.strftime('%e') + " "
+    end
+  end
+end
+
+DateCalendar(monthfirst, monthlast)
